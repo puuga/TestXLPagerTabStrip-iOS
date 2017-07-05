be99ec6b43ffcfda10cd93cd5c850b946f871185
Siwawes Wongcharoen <siwaoh@gmail.com>
2017-07-05 16:01:53 +0700

    Initial Commit

---
+++ TestXLPagerTabStrip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestXLPagerTabStrip
+//
+//  Created by Siwawes Wongcharoen on 7/5/2560 BE.
+//  Copyright © 2560 Siwawes Wongcharoen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
